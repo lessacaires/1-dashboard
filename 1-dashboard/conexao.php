ef93b70ef9ae090f7df27267009c542382e8fcb9
dennislessa <dennislessa@genesis.home>
2017-12-01 18:08:22 -0300

    teste commit.

--- 1-dashboard/conexao.php
+++ 1-dashboard/conexao.php
@@ -1,3 +1,3 @@
 <?php
-	$con = mysqli_connect("localhost","root","","teste")or die("Erro ao realizar a conexão!".mysqli_error());
-?>
+
+$con = mysqli_connect("localhost", "root", "", "teste")or die("Erro ao realizar a conexão!" . mysqli_error());
