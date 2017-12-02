6dd3610c0ce864ba2d6fcc7e4abe6c594daaa0ef
dennislessa <dennislessa@genesis.home>
2017-12-02 15:30:14 -0300

    Preparando arquivos de configuração - v1.2.2

--- config/variaveis.php
+++ config/variaveis.php
@@ -1,6 +1,6 @@
 <?php
 
 /* Constantes para uso geral */
-define('URI', filter_input(INPUT_SERVER, 'REQUEST_URI'));
-define('DOCUMENT_ROOT', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/1-dashboard');
-define('BASE', '//' . filter_input(INPUT_SERVER, 'HTTP_HOST') . '/1-dashboard');
+define('URI', dirname(filter_input(INPUT_SERVER, 'REQUEST_URI')));
+define('DOCUMENT_ROOT', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . URI);
+define('BASE', '//' . filter_input(INPUT_SERVER, 'HTTP_HOST') . URI);
