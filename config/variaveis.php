7eff5e3a5969c4fd4ebfde9f33f13087c4440639
dennislessa <dennislessa@genesis.home>
2017-12-02 15:21:26 -0300

    Aprimoramento do sistema - v1.2.1

--- config/variaveis.php
+++ config/variaveis.php
@@ -2,5 +2,5 @@
 
 /* Constantes para uso geral */
 define('URI', filter_input(INPUT_SERVER, 'REQUEST_URI'));
-define('DOCUMENT_ROOT', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/dashboard');
-define('BASE', '//' . filter_input(INPUT_SERVER, 'HTTP_HOST') . '/dashboard');
+define('DOCUMENT_ROOT', filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/1-dashboard');
+define('BASE', '//' . filter_input(INPUT_SERVER, 'HTTP_HOST') . '/1-dashboard');
